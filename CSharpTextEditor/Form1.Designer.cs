7a66735100c99001e030899a43452a8d5fafaf43
Stoyanov <stoyanov.karlovo@gmail.com>
2022-10-13 19:53:04 +0300

    Add project files.

---
+++ CSharpTextEditor/Form1.Designer.cs
@@ -0,0 +1,65 @@
+﻿namespace CSharpTextEditor
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.HtmlViewer = new System.Windows.Forms.WebBrowser();
+            this.SuspendLayout();
+            // 
+            // HtmlViewer
+            // 
+            this.HtmlViewer.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.HtmlViewer.Location = new System.Drawing.Point(12, 56);
+            this.HtmlViewer.MinimumSize = new System.Drawing.Size(20, 20);
+            this.HtmlViewer.Name = "HtmlViewer";
+            this.HtmlViewer.Size = new System.Drawing.Size(776, 382);
+            this.HtmlViewer.TabIndex = 0;
+            this.HtmlViewer.DocumentCompleted += new System.Windows.Forms.WebBrowserDocumentCompletedEventHandler(this.HtmlViewer_DocumentCompleted);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.HtmlViewer);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.DoubleClick += new System.EventHandler(this.Form1_DoubleClick);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.WebBrowser HtmlViewer;
+    }
+}
+
---
+++ CSharpTextEditor/Form1.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Windows.Input;
+using mshtml;
+using System.Text.RegularExpressions;
+
+namespace CSharpTextEditor
+{
+    public partial class Form1 : Form
+    {
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void onEnableScrollPage(object sender, EventArgs e)
+        {
+            MessageBox.Show("resized");
+        }
+
+        private void CheckForOverflowChange(HtmlElement htmlElement)
+        {
+            if (htmlElement.ScrollRectangle.Height > htmlElement.ClientRectangle.Height)
+            {
+                // add overflow appear handling code
+                htmlElement.SetAttribute("-custom-scrollbar-visible", "true");
+            }
+            else if (htmlElement.GetAttribute("-custom-scrollbar-visible").Equals("true"))
+            {
+                // add overflow disappear handling code
+                MessageBox.Show("scroll bar disappear");
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            this.BringToFront();
+            this.Focus();
+            HtmlViewer.DocumentText = 
+                "<html>" +
+                "<body style=\"background-color: gray;\">" +
+                    "<div class=\"page-container\" style=\"background-color: white;\">" +
+                            "<div id=\"page-body\" style=\"position: relative; padding: 1cm; height: 300px; overflow-y: auto; word-wrap: break-word;\">" +
+                            "</div>"+
+                    "</div>" +
+                "</body>" +
+                "</html>";
+        }
+
+        protected override bool ProcessCmdKey(ref Message msg, Keys keyData)
+        {
+            char keyCode = (char)msg.WParam;
+            bool useCaps = Control.IsKeyLocked(Keys.CapsLock) ^ Control.ModifierKeys.HasFlag(Keys.Shift);
+
+            if (!useCaps)
+                keyCode = Char.ToLower(keyCode);
+
+            HtmlElement page = HtmlViewer.Document.GetElementById("page-body");
+            page.InnerHtml += keyCode;
+
+            CheckForOverflowChange(page);
+
+            return true;
+        }
+
+
+        private void Form1_DoubleClick(object sender, EventArgs e)
+        {
+            IHTMLDocument2 doc = (IHTMLDocument2)HtmlViewer.Document.DomDocument;
+            IHTMLElement page = (IHTMLElement)HtmlViewer.Document.GetElementById("page-body").DomElement;
+
+            IHTMLTxtRange selectedRange = (IHTMLTxtRange)doc.selection.createRange();
+            selectedRange.moveToPoint(0, 0);
+            selectedRange.collapse(true);
+
+            IDisplayPointer display;
+            ((IDisplayServices)doc).CreateDisplayPointer(out display);
+
+            uint result;
+            tagPOINT point;
+            point.x = 135;
+            point.y = 135;
+
+            display.moveToPoint(point, _COORD_SYSTEM.COORD_SYSTEM_CONTENT, page, 0, out result);
+
+            IHTMLCaret caret;
+            ((IDisplayServices)doc).GetCaret(out caret);
+            caret.MoveCaretToPointer(display, 1, _CARET_DIRECTION.CARET_DIRECTION_FORWARD);
+            caret.Show(1);
+        }
+
+        private void HtmlViewer_DocumentCompleted(object sender, WebBrowserDocumentCompletedEventArgs e)
+        {
+        }
+    }
+}
